30d8a70f3ceb6e1c682c930653d401b6367080e6
RudVlad473 <72340706+RudVlad473@users.noreply.github.com>
2020-11-23 12:59:38 +0200

    lab

--- Lab7/Lab7.cpp
+++ Lab7/Lab7.cpp
@@ -10,16 +10,17 @@ int main(void)
     {
         printf("|   x   |      y     |\n");
         printf("|-------|------------|\n");
-        for (x = 0; x < 4; x += 0.25) {
+        for (x = 0; x < 4; x += 0.25) 
+        {
             if (x < 1) y = 0;
             else if (x < 2) y = -sqrt(1 - (x - 2) * (x - 2));
             else if (x < 3) y = -1;
             else if (x < 4) y = -sqrt(1 - (x - 3) * (x - 3));
             printf("| %5.2lf | %10.7lf |", x + n * 4, y);
             h = (y + 1) * 10;
-            for (; h > 0; h--) printf("");
+            if (y - 1 - h * 10 > 0.5) h++;
+            for (; h > 0; h--) printf(" ");
             printf("*\n");
         } 
     } 
-    return 0;
 }
