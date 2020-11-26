94cc405a7a153d75ec067858aa12b98205b625db
RudVlad473 <72340706+RudVlad473@users.noreply.github.com>
2020-11-26 12:29:31 +0200

    lab8

---
+++ Lab8/1/1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+
+int main()
+{
+
+			setlocale(LC_ALL, "ru");
+			int const SIZE = 200;
+			srand(time(NULL));
+			int arr[SIZE];
+			int sum = 0, sredarif;
+			for (int i = 0; i < SIZE; i++)
+			{
+				arr[i] = rand() % 20;
+			}
+			for (int i = 0; i < SIZE; i++)
+			{
+				cout << arr[i] << " ";
+				if (i == 0)
+				{
+					continue;
+				}
+				if (i % 20 == 0)
+				{
+					cout << endl;
+				}
+			}
+			for (int i = 0; i < SIZE; i++)
+			{
+				sum = sum + arr[i];
+			}
+			sredarif = sum / 200;
+			cout << endl << endl << "Сумма элементов массива = " << sum << endl << "Среднее арифметическое элементов массива = " << sredarif << endl << endl;
+			for (int i = 0; i < SIZE; i++)
+			{
+				if ((arr[i] >= 0) && (arr[i + 1] >= 0) && (arr[i] < sredarif))
+				{
+					arr[i] = sredarif;
+				}
+				cout << arr[i] << " ";
+				if (i == 0)
+				{
+					continue;
+				}
+				if (i % 20 == 0)
+				{
+					cout << endl;
+				}
+			}
+
+
+}
+
+
---
+++ Lab8/c/c.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "Введите число: ";
+	int a, n=0 , s = 0, r,k;
+	cin >> a;
+	while (a % 10 != 0)
+	{
+		a /= 10;
+		n++;
+	}
+	cout << "Кол-во цифр в числе: " << n << endl;
+	for (int s = 0; a != 0; s += a % 10)
+	{
+		a /= 10;
+	}
+	cout << "Сумма цифр числа: " << s << endl;
+}
