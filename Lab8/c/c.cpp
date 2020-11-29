6ccdf87c2b91895da82045907446dae2d7e66c25
RudVlad473 <72340706+RudVlad473@users.noreply.github.com>
2020-11-29 22:26:53 +0200

    lab

--- Lab8/c/c.cpp
+++ Lab8/c/c.cpp
@@ -2,19 +2,42 @@
 using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	cout << "Введите число: ";
-	int a, n=0 , s = 0, r,k;
-	cin >> a;
-	while (a % 10 != 0)
+	while (true)
 	{
-		a /= 10;
-		n++;
+		setlocale(LC_ALL, "ru");
+		cout << "Введите число: ";
+		int a, n = 0, r, k;
+		cin >> a;
+		/*1*/
+		while (a % 10 != 0)
+		{
+			a /= 10;
+			n++;
+		}
+		cout << "Кол-во цифр в числе: " << n << endl;
+		/*2*/
+		int x, y;
+		int sum = 0;
+		cout << "Введите число: ";
+		cin >> x;
+		for (; x > 0;) {
+			y = x % 10;
+			sum += y;
+			x /= 10;
+		}
+		cout << "Сумма цифр числа: " << sum << endl;
+		/*3*/
+		cout << "Введите число: ";
+		cin >> x;
+		for (; x > 0;)
+		{
+			y = x % 10;
+			sum += y;
+			x /= 10;
+			cout << y;
+
+		}
+		cout << endl << endl;;
 	}
-	cout << "Кол-во цифр в числе: " << n << endl;
-	for (int s = 0; a != 0; s += a % 10)
-	{
-		a /= 10;
-	}
-	cout << "Сумма цифр числа: " << s << endl;
+
 }
