e731b940fe5ed57c2823357320b61acc62235318
RudVlad473 <72340706+RudVlad473@users.noreply.github.com>
2020-12-14 12:23:26 +0200

    lab

---
+++ Lab10/1/1.cpp
@@ -0,0 +1,79 @@
+﻿
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int const N = 9;
+int const M = 9;
+int j;
+int i;
+void foo(int arr[]);
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	int arr[N * M]{};
+	for (i = 0; i < N * M; i++)
+	{
+		arr[i] = i+1;
+	}
+	cout << "Одномерный массив:" << endl;
+	for (i = 0; i < N * M; i++)
+	{	
+		cout<<setw(4) <<arr[i];
+	}
+	cout << endl << endl << endl << endl;
+	foo(arr);
+	
+	
+	
+}
+void foo (int arr[])
+{
+	int arr4[N*M];
+	int arr3[N][M];
+	cout << "Двумерный массив" << endl;
+	int arr2[N][M]{};
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++) {
+			arr2[i][j] = arr[i * N + j];
+			cout <<setw(4)<< arr2[i][j];
+		}
+		cout << endl;
+	}
+	
+	cout << endl << endl << endl << endl;
+	cout << "Обработанный двумерный массив: " << endl;
+	for (i = 0; i < N; i++) 
+	{	
+		
+		for (j = 0; j < M; j++)
+		{
+			int k = 0;
+			int t = 0;
+			k = arr2[j][j];
+			t = arr2[i][j];
+			t = t * k;
+			arr3[i][j] = t;
+
+			cout << setw(8) << t;
+		}
+		
+		cout << endl;
+	}
+	
+	cout << endl << endl << endl << endl;
+	cout << "Обработанный одномерный массив: " << endl;
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			arr4[i * M + j] = arr3[i][j];
+			cout  << arr4[i*M+j]<< " ";
+		}
+		
+	}
+
+}
+
+
---
+++ Lab10/a/a.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+int a;
+int b;
+char foo(int a, int b);
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "Введите 2 числа: ";
+	int a;
+	int b;
+	cin >> a;
+	cout << endl;
+	cin >> b;
+	cout << endl;
+	cout <<foo(a,b);
+	
+}
+char foo(int a, int b)
+{
+	if (a > b)
+	{
+		return '>';
+	}
+	else if (a == b)
+	{
+		return '=';
+	}
+	else {
+		return '<';
+	}
+}
+
+
