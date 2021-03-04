028edfdf23728f418367dfd79945b0696a7e0ae7
RudVlad473 <72340706+RudVlad473@users.noreply.github.com>
2021-03-04 21:08:36 +0200

    lab

---
+++ Lab3(2)/Header.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE  20
+int pe = 0;
+int modd = 0;
+int me = 0;
+
---
+++ Lab3(2)/Lab3(2).cpp
@@ -0,0 +1,67 @@
+﻿#include "Header.h"
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+	int Array[SIZE]{};
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+		 *(Array+i) = rand() % 200 - 100;
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (i % 5 == 0)
+		{
+			cout << endl;
+		}
+		cout << Array[i]<<"\t" ;
+
+	}
+	cout << endl<< endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		if ((*(Array + i) > 0) && (*(Array + i) % 2 == 0))
+		{
+			pe++;
+		}
+		if ((*(Array + i) < 0) && (*(Array + i) % 2 != 0))
+		{
+			modd++;
+		}
+		if ((*(Array + i) < 0) && (*(Array + i) % 2 == 0))
+		{
+			me++;
+		}
+	}
+
+	cout << "Кол-во положительных и четных: " << pe << endl << endl;
+	cout << "Кол-во отрицательных и нечетных: " << modd << endl << endl;
+	cout << "Кол-во отрицательных и четных: " << me << endl << endl;
+
+
+	int* arr = new int[me] {};
+	for (int i = 0, j =0; i < SIZE; i++)
+	{
+		if ((*(Array + i) < 0) && (*(Array + i) % 2 == 0))
+		{
+			*(arr+j) = *(Array + i);
+			j++;
+		}
+		
+	}
+
+	for (int i = 0; i < me; i++)
+	{
+		cout << *(arr+i)<< "\t";
+	}
+
+	delete []arr;
+	cout << endl;
+	
+
+}
+
+
