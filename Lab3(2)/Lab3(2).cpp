4d2ca9b8b3c175123b9556c9c393cc87ec2af43b
RudVlad473 <72340706+RudVlad473@users.noreply.github.com>
2021-03-13 11:41:49 +0200

    lab

--- Lab3(2)/Lab3(2).cpp
+++ Lab3(2)/Lab3(2).cpp
@@ -7,14 +7,23 @@ int main()
 	srand(time(NULL));
 
 	int Array[SIZE]{};
-	int Array2[SIZE]{};
+	int Array2[SIZE/2]{};
 
 	Filling(Array, SIZE);
 
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (i % 5 == 0)
+		{
+			cout << endl;
+		}
+		cout << setw(6) << *(Array + i);
+
+	}
+	cout << endl << endl;
 
 	Task(Array, Array2, SIZE);
 
-	
 
 }
 
