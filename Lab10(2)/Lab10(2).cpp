6db76ac86264787e5c0b6257318f2b15903f1ff5
RudVlad473 <72340706+RudVlad473@users.noreply.github.com>
2021-05-06 20:39:20 +0300

    lab

---
+++ Lab10(2)/Lab10(2).cpp
@@ -0,0 +1,36 @@
+﻿#include "Header.h"
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+#ifdef DEBUG
+    SUM sum1;
+    cout << "\nВведите значение x: ";
+    cin >> sum1.x;
+    cout << endl;
+    cout << "\nВведите значение y:";
+    cin >> sum1.y;
+    sum1.countS();
+    
+
+
+#endif
+
+#ifndef DEBUG
+
+    SUM sum1;
+    SUM* r = &sum1;
+    cout << "\nВведите значение x: ";
+    cin >> r->x;
+    cout << endl;
+    cout << "\nВведите значение y:";
+    cin >> r->y;
+    sum1.countS();
+    
+#endif
+     
+}
+
+
